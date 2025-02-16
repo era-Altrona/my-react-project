39801251bdac5bd021c000ae1b01b616d1a0d913
Alina Komerysta <alinakomerista3@gmail.com>
2025-02-16 18:55:38 +0100

    commit

---
+++ vite-project/src/components/App.jsx
@@ -0,0 +1,45 @@
+// src/components/App.jsx
+
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+    </div>
+  );
+}
+const favBooks = [
+    { id: "id-1", name: "JS for beginners" },
+    { id: "id-2", name: "React basics" },
+    { id: "id-3", name: "React Router overview" }
+];
+
+const BookList = ({ books }) => {
+    return (
+        <ul>
+            {books.map((book) => {
+                return <li>{book.name}</li>;
+            })}
+        </ul>
+    );
+};
+
+const App = () => {
+    return (
+        <>
+            <h1>Books of the week</h1>
+            <BookList books={favBooks} />
+        </>
+    );
+};
